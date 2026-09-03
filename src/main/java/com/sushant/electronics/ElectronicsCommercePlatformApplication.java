5db81f20dbd90ace43c6d8bb632ca3d5de7e700e
sushant9608379492 <sushant843120@gmail.com>
2026-09-03 23:55:04 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sushant/electronics/ElectronicsCommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sushant.electronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsCommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsCommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushant/electronics/ElectronicsCommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushant.electronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicsCommercePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
