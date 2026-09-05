6a4eb23e0cc3a8c8b61b8abcc7445331d0b1dd25
Sushant Kumar <sushant843120@gmail.com>
2026-09-06 00:25:32 +0530

    Add cart repository

---
+++ src/main/java/com/sushant/electronics/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.sushant.electronics.repository;
+
+import com.sushant.electronics.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByCustomerId(String customerId);
+}
