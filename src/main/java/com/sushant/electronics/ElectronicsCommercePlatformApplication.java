0a99f3dcd8fa20d3acf3330af401d300cd9fbd1a
Sushant Kumar <sushant843120@gmail.com>
2026-09-06 15:41:01 +0530

    Enable scheduled jobs for SAP Commerce CronJob simulation

--- src/main/java/com/sushant/electronics/ElectronicsCommercePlatformApplication.java
+++ src/main/java/com/sushant/electronics/ElectronicsCommercePlatformApplication.java
@@ -2,8 +2,10 @@ package com.sushant.electronics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ElectronicsCommercePlatformApplication {
 
 	public static void main(String[] args) {
